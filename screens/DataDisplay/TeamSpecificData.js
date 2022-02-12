16dd0e2a1bd35cca71f0b7b5013294b414048528
kaushal380 <kaushal.sambanna@gmail.com>
2022-02-12 01:15:11 -0500

    added graphs for shooters, pie chart for climb, tweaked pitscouting a little

--- screens/DataDisplay/TeamSpecificData.js
+++ screens/DataDisplay/TeamSpecificData.js
@@ -1,20 +1,340 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Dimensions, ScrollView} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {LineChart, PieChart} from "react-native-chart-kit";
 
 const TeamSpecificData = ({currentTeam, rawData, setModal}) => {
+   
+  const [matchDetails, setMatchDetails] = useState([1, 2, 3, 4]);
+  const [teleUpper, setTeleUpper] = useState([34, 5, 3, 2,39]);
+  const [teleLower, setTeleLower] = useState([34,4,2,23])
+  const [climbData, setClimbData] = useState([])
+
+  const [chartConfig, setChartConfit] = useState({
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  })
+
+  const [LineChartConfig, setLineChartConfig] = useState({
+    
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 0, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 100) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        // stroke: "#ffa726"
+        stroke: 'white'
+      }
+    
+  })
+
+  
+  useEffect(() => {
+    initSequentialCall()
+  }, [])
+
     const closeModal = () => {
         setModal(false);
     }
-  return (
-    <View style = {styles.container}>
-      <Text style = {{fontSize: 20}}>team number: {currentTeam}</Text>
-      <Text style = {{fontSize: 50}} onPress={() => console.log(rawData)}>print raw</Text>
-      <Text style = {{fontSize: 40}} onPress={closeModal}>close Modal</Text>
-      
-    </View>
-  )
-}
 
+    const initSequentialCall = () => {
+      initializeConsts()
+      setTeleUpperCargoData()
+      setTeleLowerCargoData()
+      setClimbs()
+    }
+
+    const initializeConsts = () => {
+      let raw = rawData;
+      let matches = []
+      let filteredTeamData = []
+      let recogTeam = currentTeam
+
+      for (let index = 0; index < raw.length; index++) {
+        if(raw[index].teamNum === recogTeam){
+          filteredTeamData = [...filteredTeamData, raw[index]]
+        }
+      }
+
+     
+      for (let index = 0; index < filteredTeamData.length; index++) {
+        let matchNum = parseInt(filteredTeamData[index].matchNum)
+         matches = [...matches, matchNum]
+      }
+
+      matches = matches.sort(function(a,b) {return a-b})
+      matches = [... new Set(matches)];
+      // console.log(matches)
+      let finalData = [filteredTeamData, matches]
+      return finalData;
+    }
+    const setTeleUpperCargoData = () => {
+
+      let filteredTeamData = initializeConsts()[0]
+      let matches = initializeConsts()[1]
+      let averageShootingData = []
+
+
+      setMatchDetails(matches)
+      for (let index = 0; index < matches.length; index++) {
+        let totalScore = 0;
+        let counter = 0;
+        for(let i = 0; i < filteredTeamData.length; i++){
+          if(matches[index] == filteredTeamData[i].matchNum){
+            totalScore = totalScore + filteredTeamData[i].teleUpperCargo;
+            counter++
+          }
+        }
+        let matchAvg = totalScore/counter;
+        averageShootingData = [...averageShootingData, matchAvg]      
+      }
+
+      // console.log(averageShootingData)
+      setTeleUpper(averageShootingData)
+      }
+    
+      const setTeleLowerCargoData = () => {
+
+        let filteredTeamData = initializeConsts()[0]
+        let matches = initializeConsts()[1]
+        let averageShootingData = []
+  
+        setMatchDetails(matches)
+        for (let index = 0; index < matches.length; index++) {
+          let totalScore = 0;
+          let counter = 0;
+          for(let i = 0; i < filteredTeamData.length; i++){
+            if(matches[index] == filteredTeamData[i].matchNum){
+              totalScore = totalScore + filteredTeamData[i].teleLowerCargo;
+              counter++
+            }
+          }
+          let matchAvg = totalScore/counter;
+          averageShootingData = [...averageShootingData, matchAvg]      
+        }
+  
+        setTeleLower(averageShootingData)
+        }
+
+        const setClimbs = () => {
+          let filteredTeamData = initializeConsts()[0]
+          let matches = initializeConsts()[1]
+          let Low = 0
+          let mid = 0
+          let high = 0
+          let Treversals = 0
+          let none =  0
+
+          for (let index = 0; index < matches.length; index++) {
+            let totalLow = 0
+            let totalmid = 0
+            let totalhigh = 0
+            let totalTreversals = 0
+            let totalNone = 0
+            for (let i = 0; i < filteredTeamData.length; i++) {
+              if(filteredTeamData[i].matchNum == matches[index]){
+                if(filteredTeamData[i].climb === "low"){
+                  totalLow++;
+                }
+                else if(filteredTeamData[i].climb === "mid"){
+                  totalmid++;
+                }
+                else if(filteredTeamData[i].climb === "high"){
+                  totalhigh++;
+                }
+                else if(filteredTeamData[i].climb === "traversal"){
+                  totalTreversals++;
+                }
+                else if(filteredTeamData[i].climb === "none"){
+                  totalNone++;
+                }
+              }
+            }
+            let highCheck = 0
+            let ClimbType = ""
+            let numCheck = [totalLow, totalmid, totalhigh, totalTreversals, totalNone];
+
+            let arrayCheck = [
+              {
+                type: 'low',
+                num: totalLow,
+              },
+              {
+                type: 'mid',
+                num: totalmid,
+              },
+              {
+                type: 'high', 
+                num: totalhigh,
+              },
+              {
+                type: 'traversal',
+                num: totalTreversals
+              },
+              {
+                type: 'none',
+                num: totalNone
+              }
+            ]
+            
+            for (let index = 0; index < numCheck.length; index++) {
+              if(numCheck[index]> highCheck){
+                highCheck = numCheck[index]
+              }
+            }
+            console.log("highest value:" + highCheck)
+            for (let index = 0; index < arrayCheck.length; index++) {
+              if(arrayCheck[index].num === highCheck){
+                ClimbType = arrayCheck[index].type
+                break;
+              }
+            }
+            
+            if(ClimbType === "low"){
+              Low++;
+            }
+            else if(ClimbType === "mid"){
+              mid++;
+            }
+            else if(ClimbType === "high"){
+              high++;
+            }
+            else if(ClimbType === "traversal"){
+              Treversals++;
+            }
+            else if(ClimbType === "none"){
+              none++;
+            }
+            }
+
+
+            const data = [
+              {
+                name: "None",
+                number: none,
+                color: "rgba(131, 167, 234, 1)",
+                legendFontColor: "#7F7F7F",
+                legendFontSize: 15
+              },
+              {
+                name: "Low",
+                number: Low,
+                color: "yellow",
+                legendFontColor: "#7F7F7F",
+                legendFontSize: 15
+              },
+              {
+                name: "Mid",
+                number: mid,
+                color: "red",
+                legendFontColor: "#7F7F7F",
+                legendFontSize: 15
+              },
+              {
+                name: "High",
+                number: high,
+                color: "green",
+                legendFontColor: "#7F7F7F",
+                legendFontSize: 15
+              },
+              {
+                name: "traversal",
+                number: Treversals,
+                color: "rgb(0, 0, 255)",
+                legendFontColor: "#7F7F7F",
+                legendFontSize: 15
+              }
+            ];
+            
+            setClimbData(data)
+            console.log(climbData)
+          }
+        
+
+  return (
+    <ScrollView>
+    <View style = {styles.container}>
+      <Text style = {{fontSize: 40, alignSelf: 'center', marginVertical: 30, color: '#0782F9', fontWeight: 'bold'}}>Team specific data</Text>
+      <Text style = {{fontSize: 30, alignSelf: 'flex-start', marginLeft: 20, marginBottom: 20, fontWeight: '900'}}>team number: {currentTeam}</Text>
+      <Text style = {{fontSize: 30, alignSelf: 'flex-start', marginLeft: 15, marginTop: 30, fontWeight: '900'}}>teleop data: </Text>
+      <View>
+
+      <Text style = {{alignSelf: 'center', fontSize: 20, color: 'black', marginTop: 30}}>Upper Cargo Trend</Text>
+      <LineChart
+        data={{
+          labels: matchDetails,
+          datasets: [
+            {
+              data: teleUpper
+            }
+          ]
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisInterval={5} // optional, defaults to 1
+        chartConfig={LineChartConfig}
+        // bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+
+    <Text style = {{alignSelf: 'center', fontSize: 20, color: 'black', marginTop: 20}}>Lower Cargo Trend</Text>
+      <LineChart
+        data={{
+          labels: matchDetails,
+          datasets: [
+            {
+              data: teleLower
+            }
+          ]
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisInterval={5} // optional, defaults to 1
+        chartConfig={LineChartConfig}
+        // bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+
+    </View>
+    <Text style = {{alignSelf: 'center', fontSize: 20, color: 'black', marginTop: 20}}>Overall Climb</Text>
+    <PieChart
+        data={climbData}
+        width={Dimensions.get("window").width}
+        height={250}
+        chartConfig={chartConfig}
+        accessor={"number"}
+        backgroundColor={"transparent"}
+        paddingLeft={"15"}
+        style = {{marginVertical: 0}}
+        center = {[10, 10]}
+        absolute
+    />
+    
+    <Text style = {{fontSize: 40, marginTop: 30}}>Print raw</Text>
+    <Text style = {{fontSize: 40}} onPress={closeModal}>close Modal</Text>
+    </View>
+    </ScrollView>
+  )
+
+      }
 export default TeamSpecificData
 
 const styles = StyleSheet.create({
--- screens/Pitscouting/Pitscouting.js
+++ screens/Pitscouting/Pitscouting.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import {  SafeAreaView, StyleSheet, TextInput, Text, View} from "react-native";
+import {  SafeAreaView, StyleSheet, TextInput, Text, View, ScrollView} from "react-native";
 import { Slider } from 'react-native-elements';
 
-const pitScouting = () => {
+const PitScouting = () => {
   const [text, onChangeText] = useState("Enter");
   const [text1, onChangeText1] = useState("Enter");
   const [comments, whatyougottasayaboutem] = useState("Enter");
@@ -11,31 +11,16 @@ const pitScouting = () => {
   
 
   return (
+    <ScrollView>
     <SafeAreaView>
-      <Text style={{alignSelf: 'center', fontSize: 30, fontFamily:"Cochin"}}>---Robot still being worked on---</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
-       <Text style={{alignSelf: 'center', fontSize: 30, fontFamily:"Cochin"}}>----Gracious professionalism----</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText1}
-        value={text1}
-      />
-      <Text style={{alignSelf: 'center', fontSize: 30, fontFamily:"Cochin"}}>----Extra comments----</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={whatyougottasayaboutem}
-        value={comments}
-      />
-      <Text style = {{alignSelf: 'center', marginTop: 60, fontSize: 30, marginRight: 30, fontFamily: "Cochin"}}>
+      
+      <Text style = {{alignSelf: 'center', marginTop: 20, fontSize: 30}}>
              ---- Visuals ----
          </Text>
-         <View style = {{marginTop: 40, alignContent: 'center', marginRight: 40}}> 
-         <Text style = {{fontSize: 25}}>Visual Ranking: {Visualranking}</Text>
+         <View style = {{marginTop: 35, alignContent: 'center', marginRight: 40}}> 
+         <Text style = {{fontSize: 25, marginLeft: 40}}>Visual Ranking: {Visualranking}</Text>
          <Slider
+            style = {{marginLeft: 40}}
              value = {Visualranking}
              onValueChange = {(num) => {setvisualranking(num)} }
              minimumValue = {1}
@@ -44,16 +29,41 @@ const pitScouting = () => {
              onSlidingComplete = {(num) => {setvisualranking(num)} }
              allowTouchTrack
              trackStyle = {{height : 10}}
-             thumbStyle = {{height : 20, width : 20, backgroundColor : "grey", fontFamily: "Cochin"}}
+             thumbStyle = {{height : 20, width : 20, backgroundColor : "grey"}}
          />
          </View>
+         <Text style = {styles.subHeader}>
+             ---- General Comments ----
+         </Text>
+      <Text style={styles.textBoxComment}>Robot still being worked on</Text>
+      <TextInput
+        style={styles.input}
+        multiline
+        onChangeText={onChangeText}
+        value={text}
+      />
+       <Text style={styles.textBoxComment}>Gracious professionalism</Text>
+      <TextInput
+        style={styles.input}
+        multiline
+        onChangeText={onChangeText1}
+        value={text1}
+      />
+      <Text style={styles.textBoxComment}>Extra comments</Text>
+      <TextInput
+        style={styles.input}
+        multiline
+        onChangeText={whatyougottasayaboutem}
+        value={comments}
+      />
     </SafeAreaView>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   input: {
-    height: 50,
+    height: 100,
     margin: 30,
     borderWidth: 2,
     padding: 10,
@@ -64,10 +74,19 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     fontFamily: "Cochin"
-    
-    
-    
+
+  },
+  textBoxComment: {
+    alignSelf: 'flex-start', 
+    fontSize: 20, 
+    marginLeft: 30, 
+    marginBottom: -20
+  },
+  subHeader: {
+    alignSelf: 'center', 
+    marginVertical: 40, 
+    fontSize: 30, 
   }
 });
 
-export default pitScouting;
+export default PitScouting;
