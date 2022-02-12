e6633fb49ef1b75c35f1289794441d5e965c158d
Hugendas <sohamshetty12@gmail.com>
2022-02-11 23:51:57 -0500

    (put a message on what the commit is abt)

--- screens/Pitscouting/Pitscouting.js
+++ screens/Pitscouting/Pitscouting.js
@@ -1,20 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import {  SafeAreaView, StyleSheet, TextInput, Text, View} from "react-native";
+import { Slider } from 'react-native-elements';
 
-const Pitscouting = () => {
+const pitScouting = () => {
+  const [text, onChangeText] = useState("Enter");
+  const [text1, onChangeText1] = useState("Enter");
+  const [comments, whatyougottasayaboutem] = useState("Enter");
+  const [Visualranking, setvisualranking] = useState(1);
+ 
+  
+
   return (
-    <View style = {styles.container}>
-      <Text style = {{fontSize: 20}}>Pitscouting stuff goes here</Text>
-    </View>
-  )
-}
-
-export default Pitscouting
+    <SafeAreaView>
+      <Text style={{alignSelf: 'center', fontSize: 30, fontFamily:"Cochin"}}>---Robot still being worked on---</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+       <Text style={{alignSelf: 'center', fontSize: 30, fontFamily:"Cochin"}}>----Gracious professionalism----</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText1}
+        value={text1}
+      />
+      <Text style={{alignSelf: 'center', fontSize: 30, fontFamily:"Cochin"}}>----Extra comments----</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={whatyougottasayaboutem}
+        value={comments}
+      />
+      <Text style = {{alignSelf: 'center', marginTop: 60, fontSize: 30, marginRight: 30, fontFamily: "Cochin"}}>
+             ---- Visuals ----
+         </Text>
+         <View style = {{marginTop: 40, alignContent: 'center', marginRight: 40}}> 
+         <Text style = {{fontSize: 25}}>Visual Ranking: {Visualranking}</Text>
+         <Slider
+             value = {Visualranking}
+             onValueChange = {(num) => {setvisualranking(num)} }
+             minimumValue = {1}
+             maximumValue ={5}
+             step = {1}
+             onSlidingComplete = {(num) => {setvisualranking(num)} }
+             allowTouchTrack
+             trackStyle = {{height : 10}}
+             thumbStyle = {{height : 20, width : 20, backgroundColor : "grey", fontFamily: "Cochin"}}
+         />
+         </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  input: {
+    height: 50,
+    margin: 30,
+    borderWidth: 2,
+    padding: 10,
+
+  },
+  TitleStyle: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: "bold",
+    fontFamily: "Cochin"
+    
+    
+    
+  }
+});
+
+export default pitScouting;
