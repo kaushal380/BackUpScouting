ec098e881f7c90f35f9950af3d444f011b289df9
Cyrus Yong <cyrusyong1@gmail.com>
2022-04-05 21:14:02 -0400

    bug fixes

--- screens/picturesCollect/PictureCollect.js
+++ screens/picturesCollect/PictureCollect.js
@@ -1,119 +1,98 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
 import { CameraComponent } from "../picturesCollect/Camera";
-import { AntDesign, Entypo } from "@expo/vector-icons"
-import { getStorage, ref, uploadBytes, getDownloadURL, uploadBytesResumable } from 'firebase/storage';
-import { useNavigation } from '@react-navigation/core';
+import { AntDesign, Entypo } from "@expo/vector-icons";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { useNavigation } from "@react-navigation/core";
+
 const PictureCollect = () => {
-    const navigation = useNavigation()
-    const [image, setImage] = useState();
-    const [team, setTeam] = useState();
-    const pictureSubmit = async () => {
-        if(!team){
-            alert("enter the team number")
-            return;
-        }
-        let url = ""
-        try {
-            const storage = getStorage();
-            // const ref = ref(storage, 'image.png');
-            let name = team + ".jpg"
-            const reference = ref(storage, name)
-            // const img = await fetch(image);
-            // const bytes =
-            const img = await fetch(image);
-            const bytes = await img.blob();
-            await uploadBytesResumable(reference, bytes)
-            url = (await getDownloadURL(reference)).toString()
-            console.log(url)
-            navigation.navigate('Home');
-        }
-        catch (e) {
-            console.log(e)
-        }
+  const navigation = useNavigation();
+  const [image, setImage] = useState("");
+  const [team, setTeam] = useState("");
+
+  const storage = getStorage();
+  const storageRef = ref(storage, `${team}`);
+
+  async function uploadImageAsync(uri) {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+    await uploadBytes(storageRef, blob);
+    blob.close();
+    return await getDownloadURL(storageRef);
+  }
+
+  const pictureSubmit = async () => {
+    console.log("submitting");
+    if (!team) {
+      alert("enter the team number");
+      return;
     }
-    return (
-        <View style={styles.container}>
-            <ScrollView>
-                <TextInput
-                    placeholder="Team #"
-                    keyboardType='numbers-and-punctuation'
-                    value={team}
-                    onChangeText={text => setTeam(text)}
-                    style={styles.Teaminput}
-                />
-                <CameraComponent image={image} setImage={setImage} />
-                <TouchableOpacity style={styles.ButtonsContainer} onPress={pictureSubmit}>
-                    <Text style={{ color: "white" }}>Submit</Text>
-                </TouchableOpacity>
-            </ScrollView>
-        </View>
-
-
-
-    )
-}
-
-export default PictureCollect
-
+    uploadImageAsync(image).then((url) => {
+      console.log(url);
+      navigation.navigate("Home");
+    });
+  };
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <TextInput
+          placeholder="Team #"
+          keyboardType="numbers-and-punctuation"
+          value={team}
+          onChangeText={(text) => setTeam(text)}
+          style={styles.Teaminput}
+        />
+        <CameraComponent image={image} setImage={setImage} />
+        <TouchableOpacity
+          style={styles.ButtonsContainer}
+          onPress={pictureSubmit}
+        >
+          <Text style={{ color: "white" }}>Submit</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+};
+export default PictureCollect;
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
-        flex: 1,
-        alignItems: 'center',
-    },
-    Teaminput: {
-        height: 50,
-        margin: 30,
-        borderWidth: 2,
-        padding: 10,
-        minWidth: 50,
-    },
-    ButtonsContainer: {
-        backgroundColor: "#0782F9",
-        width: 200,
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 50,
-        borderRadius: 10,
-        marginTop: 50
-    },
-})
-
-// import React, { useState } from 'react';
-// import { View, TouchableOpacity, StyleSheet, Text, Alert, Dimensions, Image } from 'react-native';
-// import { CameraComponent } from "../picturesCollect/Camera";
-// import { getStorage, ref, uploadBytes, getDownloadURL, uploadBytesResumable } from 'firebase/storage'
-// import { useNavigation } from '@react-navigation/core';
-
-// const PictureCollect = () => {
-//     const navigation = useNavigation();
-//     const [image, setImage] = useState();
-//     const handlePictureUpload = async() => {
-//         let url = ""
-//         try {
-//             const storage = getStorage();
-//             // const ref = ref(storage, 'image.png');
-//             let name = team + ".jpg"
-//             const reference = ref(storage, name)
-//             // const img = await fetch(image);
-//             // const bytes =
-//             const img = await fetch(image);
-//             const bytes = await img.blob();
-//             await uploadBytesResumable(reference, bytes)
-//             url = (await getDownloadURL(reference)).toString()
-//             console.log(url)
-//         }
-//         catch (e) {
-//             console.log(e)
-//         }
-//     }
-//     return(
-//         <View>
-//             <Text>Hello</Text>
-//         </View>
-//     )
-// }
-// export default PictureCollect;
-
-
+  container: {
+    justifyContent: "center",
+    flex: 1,
+    alignItems: "center",
+  },
+  Teaminput: {
+    height: 50,
+    margin: 30,
+    borderWidth: 2,
+    padding: 10,
+    minWidth: 50,
+  },
+  ButtonsContainer: {
+    backgroundColor: "#0782F9",
+    width: 200,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    borderRadius: 10,
+    marginTop: 50,
+  },
+});
